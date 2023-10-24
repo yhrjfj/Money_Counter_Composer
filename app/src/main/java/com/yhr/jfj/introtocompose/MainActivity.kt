d91f8810442b60019c425182391b0b732225c5af
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-24 17:18:34 +0600

    Create MyApp, CreateCircle function

--- app/src/main/java/com/yhr/jfj/introtocompose/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/introtocompose/MainActivity.kt
@@ -3,14 +3,18 @@ package com.yhr.jfj.introtocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yhr.jfj.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,17 +27,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MyApp()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+// Main Function
+@Composable
+fun MyApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF546E7A)
+    ) {
+        CreateCircle()
+    }
+}
+
+// Circle which act as a button
+@Preview
+@Composable
+fun CreateCircle() {
+    Card(
+        modifier = Modifier.padding(3.dp).size(45.dp),
+        shape = CircleShape
+    ) {
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center) {
+            Text("Tap")
+        }
+    }
+}
+
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntroToComposeTheme {
-
+        MyApp()
     }
 }
